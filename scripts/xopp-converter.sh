30f5366c5eb1ce162c4f8eb41ddc29e0b57d5dd9
ginop-1 <henrycovili2004@gmail.com>
2024-04-03 16:32:47 +0200

    test update lezioni

--- scripts/xopp-converter.sh
+++ scripts/xopp-converter.sh
@@ -4,10 +4,10 @@ IFS=$'\n'
 
 for file in $(find $(git root) -type f -name "*.xopp")
 do
-    if [ -f "${file%.xopp}.pdf" ]; then
+    # if [ -f "${file%.xopp}.pdf" ]; then
         # echo "File ${file%.xopp}.pdf already exists. Skipping ${file}"
-        continue
-    fi
+        # continue
+    # fi
     # echo $file >> test.txt
     xournalpp -p "${file%.xopp}.pdf" "${file}"
 done
