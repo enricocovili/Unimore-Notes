6f6afb6a8143b8a751cfb85d69c877120c73abf9
enricocovili <henrycovili2004@gmail.com>
2025-11-24 19:35:39 +0100

    fixed image script

--- scripts/fix_screenshots.sh
+++ scripts/fix_screenshots.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-mkdir imgs > /dev/null 2>&1
-mv *.png imgs/ > /dev/null 2>&1
-# replace all spaces in imgs folder with underscores
-cd imgs
-find . -depth -name '* *' \
-| while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
-cd ..
+ROOT_DIR=$(pwd)
+echo "executing $0 at $ROOT_DIR"
 
-# regex da ricordare
-# \.png\]\] -> .png)                                                .png]] -> .png)
-# !\[\[Pasted image -> ![placeholder](./imgs/Pasted_image_          ![[Pasted image  -> ![placeholder](./imgs/Pasted_image_
+find . -type d | while IFS= read -r d
+do
+    cd "$ROOT_DIR"
+    cd "$d"
+    if [ ! -d "imgs" ]; then
+        continue
+    fi
+    mv *.png imgs/ > /dev/null 2>&1
+    # replace all spaces in imgs folder with underscores
+    cd imgs
+    find . -depth -name '* *' \
+    | while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
+    cd ..
 
-# replace all .png]] with .png)
-sed -i 's/\.png\]\]/.png)/g' *.md
-# replace all ![[Pasted image with ![placeholder](./imgs/Pasted_image_
-sed -i 's/!\[\[Pasted image /![placeholder](\.\/imgs\/Pasted_image_/g' *.md
+    # regex da ricordare
+    # \.png\]\] -> .png)                                                .png]] -> .png)
+    # !\[\[Pasted image -> ![placeholder](./imgs/Pasted_image_          ![[Pasted image  -> ![placeholder](./imgs/Pasted_image_
+
+    # replace all .png]] with .png)
+    sed -i 's/\.png\]\]/.png)/g' *.md
+    # replace all ![[Pasted image with ![placeholder](./imgs/Pasted_image_
+    sed -i 's/!\[\[Pasted image /![placeholder](\.\/imgs\/Pasted_image_/g' *.md
+done
